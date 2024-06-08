3aea443116eb8496d67d03c58ca3d77a4b4d9587
Abe Manfra <abemanfra@gmail.com>
2024-06-08 18:36:16 -0400

    Can push blocks left and right now.

--- script.lua
+++ script.lua
@@ -28,12 +28,12 @@ local function generateBoard(height, width)
 end
 
 
-local function objectAt(postion)
-    return board[#board - postion[2]+1][postion[1]];
+local function objectAt(position)
+    return board[#board - position[2]+1][position[1]];
 end
 
-local function setObjectAt(postion, object)
-    board[#board - postion[2]+1][postion[1]] = object;
+local function setObjectAt(position, object)
+    board[#board - position[2]+1][position[1]] = object;
 end
 
 
@@ -51,7 +51,7 @@ local function canMoveTo(postion)
 end
 
 local function canMoveBlock(block_position, amount_to_move_by)
-    future_postion = {block_position[1] + amount_to_move_by[1], block_position[2] + amount_to_move_by[2]};
+    local future_postion = {block_position[1] + amount_to_move_by[1], block_position[2] + amount_to_move_by[2]};
     print(string.format("Testing (%d, %d)", future_postion[1], future_postion[2]));
     if (not canMoveTo(future_postion)) then
         return false;
@@ -68,16 +68,47 @@ local function getRow(position)
     return board[translated_position[2]]; 
 end
 
-local function moveBlock(block_position, amount_to_move_by)
+local function getCol(position) 
+    local output = {}; 
+    for i=1, #board do
+        table.insert(output,objectAt({position[1], i}));
+    end
+    return output;
+end
+
+local function moveBlock(player_position, amount_to_move_by)
     --[[
         - Get direction
         - Loop through until non block char
         - Set all to blocks between player pos and the empty pos.
-    
-    if player_position[1] ~= 0 then
-        
-    elseif player_position[2] ~=0 then
-    end]]
+    ]]
+    if amount_to_move_by[1] == 1 then
+        local row = getRow(player_position);
+        for i=1,#row do
+            if objectAt({i,player_position[2]}) == empty_space_character then
+                setObjectAt({#board-i,player_position[2]}, block_character);
+                return;
+            end
+        end
+
+    elseif amount_to_move_by[1] == -1 then
+        local row = getRow(player_position);
+        for i=player_position[1],1,-1 do
+            if objectAt({i,player_position[2]}) == empty_space_character then
+                setObjectAt({i,player_position[2]}, block_character);
+                return;
+            end
+        end
+    elseif amount_to_move_by[2] == 1 then
+        local col = getCol(player_position);
+        for i=#board-player_position[1] +1,#board,1 do
+            print(string.format("Testing (%d,%d), %s.",player_position[1], i, objectAt({player_position[1], i})));
+            if objectAt({player_position[1],i}) == empty_space_character then
+                setObjectAt({player_position[1],#board-i+1}, block_character);
+                return;
+            end
+        end
+    end
 end   
 
 local function setPlayerPositon(new_postion)
@@ -85,17 +116,15 @@ local function setPlayerPositon(new_postion)
     player_position = new_postion;
 end
 
-local function move(x,y)
-    local future_postion = {player_position[1] + x, player_position[2] + y};
-    print(string.format("Attempted to move to: (%d, %d)", future_postion[1], future_postion[2]));
-
+local function move(amount_to_move_by)
+    local future_postion = {player_position[1] + amount_to_move_by[1], player_position[2] + amount_to_move_by[2]};
     if (not canMoveTo(future_postion)) then
         return;
     elseif objectAt(future_postion) == block_character then
-        if (canMoveBlock(future_postion, {x,y})) then 
-            print("Hello World, this is a test.")
-            moveBlock(future_postion, {x,y})
+        if (not canMoveBlock(future_postion, amount_to_move_by)) then 
             return;
+        else
+            moveBlock(player_position,amount_to_move_by);
         end
     end
     setObjectAt(player_position, empty_space_character);
@@ -126,24 +155,14 @@ local function main()
     setObjectAt({5,5},wall_character);
     setObjectAt({6,5},wall_character);
     setObjectAt({7,5},wall_character);
-    setObjectAt({3,4}, block_character);
-    setObjectAt({2,4}, block_character);
-
-    setObjectAt({2,3}, block_character);
-    setObjectAt({1,3}, block_character);
-    setObjectAt({10,3}, block_character);
+    setObjectAt({8,4}, block_character);
+    setObjectAt({7,4}, block_character);
     --setObjectAt({1,4}, block_character);
+
+    
     
     
     renderBoard();
-
-    local test = getRow(player_position);
-    local output = "";
-
-    for i=1,#test do
-        output = output .. test[i]; 
-    end
-    print(output);
     
 
 end
@@ -152,21 +171,21 @@ main()
 
 if (not tty_mode) then
     get("up").on_click(function()
-        move(0,1);
+        move({0,1});
         renderBoard()
         
     end)
     get("down").on_click(function()
-        move(0,-1);
+        move({0,-1});
         renderBoard();
     end)
     get("left").on_click(function()
-        move(-1,0);
+        move({-1,0});
         renderBoard();
 
     end)
     get("right").on_click(function()
-        move(1,0);
+        move({1,0});
         renderBoard();
 
     end)
@@ -176,16 +195,16 @@ else
     while true do 
         next_input = string.lower(io.read())
         if next_input == "up" then
-            move(0,1);
+            move({0,1});
             renderBoard();
         elseif next_input == "down" then
-            move(0,-1);
+            move({0,-1});
             renderBoard()
         elseif next_input == "left" then
-            move(-1,0);
+            move({-1,0});
             renderBoard()
         elseif next_input == "right" then
-            move(1,0);
+            move({1,0});
             renderBoard()
         else
             error("Command not found.");
